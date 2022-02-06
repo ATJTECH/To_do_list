d4ad03c30e97eee8b15f4a926b8a6c5192e39cb0
ATJTECH <angeltjohn@gmail.com>
2022-02-06 17:26:45 +0530

    commit

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express =require('express')
 const taskcontroller = require(''./controllers/taskcontroller')
 const path=require('path')
 const exphbs=require('express-handlebars')
+const bodyparse=require('body-parser')
 
 var app=express();
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
 app.set('views',path.join(__dirname,'/views/'))
 app.engine('hbs' , exphbs.engine({extname:'hbs',defaultLayout:'mainLayout',layoutsDir :__dirname +'/views/layouts'}))
 app.set('view engine','hbs')
 app.listen(7000,()=>{
     console.log('express server started')
 })
+
 app.use('/task',taskcontroller)
